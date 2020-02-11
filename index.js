a7511a230b44b352083db6eb0cb6d671e1b0bf20
thongho98 <hoquocthong456@gmail.com>
2020-02-11 18:45:03 +0700

    Static layout

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.set('views','./views');
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+app.use(express.static('public'));
+
 app.get('/',function(request,reponse) {
     reponse.render('index',{
         name: 'THONG'
