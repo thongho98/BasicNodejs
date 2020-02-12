1c22071255c7b54c520961f88f3afd5662f346dc
thongho98 <hoquocthong456@gmail.com>
2020-02-12 09:31:25 +0700

    Tach validate rieng ra goi bang middleware next

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 var express = require('express');
 
 var controller = require('../controllers/user.controller');
+var validate = require('../validate/user.validate')
 
 var router = express.Router();
 
@@ -11,7 +12,7 @@ router.get('/search',controller.search);
 //Form dang ki
 router.get('/create', controller.formCreateUser);
 
-router.post('/create', controller.createUser);
+router.post('/create', validate.validateUser, controller.createUser);
 
 //View từng user
 router.get('/:id',controller.getUser);
